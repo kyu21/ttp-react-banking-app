e4ab86640d40c662c51dc96c946406e205962d23
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-11 03:55:37 -0500

    Updates

---
+++ precache-manifest.601e2cf02aa6df14d8309e30264b7e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b656a0e6ff9b7b7d8b6eed83618ed192",
+    "url": "/ttp-react-banking-app/index.html"
+  },
+  {
+    "revision": "9a600228996d19728a88",
+    "url": "/ttp-react-banking-app/static/css/main.22156b0c.chunk.css"
+  },
+  {
+    "revision": "1f4af7530f9a4d82f4a4",
+    "url": "/ttp-react-banking-app/static/js/2.8c023e59.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ttp-react-banking-app/static/js/2.8c023e59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a600228996d19728a88",
+    "url": "/ttp-react-banking-app/static/js/main.1e694dc1.chunk.js"
+  },
+  {
+    "revision": "45a3de033bd14c771b3b",
+    "url": "/ttp-react-banking-app/static/js/runtime-main.b17c4a62.js"
+  }
+]);
